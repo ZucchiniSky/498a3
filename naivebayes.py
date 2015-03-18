0c902796e5dca0ad2d95745a813d74589851b450
ZucchiniSky <scottbommarito@gmail.com>
2015-03-18 12:34:38 -0400

    vocabTrue vocabFalse

--- naivebayes.py
+++ naivebayes.py
@@ -6,36 +6,51 @@ from os.path import isfile, join
 
 processedFiles = {}
 bayesData = [0, 0, {}, {}]
-vocab = 0
+vocabTrue = 0
+vocabFalse = 0
 
 def trainNaiveBayes(files):
     global bayesData
     global processedFiles
-    global vocab
+    global vocabTrue
+    global vocabFalse
     bayesData = [0, 0, {}, {}]
     bayesData[1] = len(files)
-    vocab = 0
-    tokens = set()
+    vocabTrue = 0
+    vocabFalse = 0
+    tokensTrue = set()
+    tokensFalse = set()
     for filename in files:
         index = 3
+        tokenSet = processedFiles[filename]
         if fileIsTruth(filename):
             index = 2
             bayesData[0] += 1
-        tokenSet = processedFiles[filename]
+            for token in tokenSet:
+                tokensTrue.add(token)
+        else:
+            for token in tokenSet:
+                tokensFalse.add(token)
         for token in tokenSet:
             if bayesData[index].get(token) is None:
                 bayesData[index][token] = 0
             bayesData[index][token] += 1
-            tokens.add(token)
-    vocab = len(tokens)
+    vocabTrue = len(tokensTrue)
+    vocabFalse = len(tokensFalse)
 
-def calcProbability(index, classnum, docnum, tokens):
-    global vocab
+def calcProbability(index, classnum, docnum, tokens, truth):
+    global vocabTrue
+    global vocabFalse
     prob = float(classnum) / docnum
     for token in tokens:
         tokenCount = 0
         if index.get(token) is not None:
             tokenCount = index[token]
+        vocab = 0
+        if truth:
+            vocab = vocabTrue
+        else:
+            vocab = vocabFalse
         prob *= float(tokenCount + 1) / float(classnum + vocab)
     return prob
 
@@ -43,8 +58,8 @@ def testNaiveBayes(file):
     global bayesData
     global processedFiles
     tokenSet = processedFiles[file]
-    truthProb = calcProbability(bayesData[2], bayesData[0], bayesData[1], tokenSet)
-    lieProb = calcProbability(bayesData[3], bayesData[1] - bayesData[0], bayesData[1], tokenSet)
+    truthProb = calcProbability(bayesData[2], bayesData[0], bayesData[1], tokenSet, True)
+    lieProb = calcProbability(bayesData[3], bayesData[1] - bayesData[0], bayesData[1], tokenSet, False)
     if truthProb > lieProb:
         return True
     else:
